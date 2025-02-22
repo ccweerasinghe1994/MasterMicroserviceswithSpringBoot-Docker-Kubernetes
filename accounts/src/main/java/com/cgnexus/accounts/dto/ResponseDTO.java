33831c468b1f06fe69d82a228d76f1d620351a82
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-02-22 21:13:46 +0530

    DTOs

---
+++ accounts/src/main/java/com/cgnexus/accounts/dto/ResponseDTO.java
@@ -0,0 +1,15 @@
+package com.cgnexus.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDTO {
+
+    private String statusCode;
+
+    private String statusMessage;
+
+    
+}
