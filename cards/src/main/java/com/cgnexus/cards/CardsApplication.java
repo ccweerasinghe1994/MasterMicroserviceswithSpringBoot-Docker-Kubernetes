84f623feda24c17b3fe95fb11801dec15aaee5c0
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-03-11 10:27:20 +0530

    add OpenAPI annotations for Cards Microservice API documentation

--- cards/src/main/java/com/cgnexus/cards/CardsApplication.java
+++ cards/src/main/java/com/cgnexus/cards/CardsApplication.java
@@ -1,9 +1,29 @@
 package com.cgnexus.cards;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards Microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "CGNexus",
+						email = "ccweerasinghe1994@gmail.com",
+						url = "api.cgnexus.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "http://www.apache.org/licenses/LICENSE-2.0"
+				),
+				description = "This is a RESTful API documentation for the Cards Microservice."
+		)
+)
 public class CardsApplication {
 
 	public static void main(String[] args) {
