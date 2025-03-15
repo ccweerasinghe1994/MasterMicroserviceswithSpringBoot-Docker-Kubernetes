1e688a04dab3456496ffcfa30e2b3cdf7714d73a
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-03-15 16:02:10 +0530

    refactor contact info DTOs to use class structure with Lombok annotations

--- cards/src/main/java/com/cgnexus/cards/dto/CardsContactInfoDTO.java
+++ cards/src/main/java/com/cgnexus/cards/dto/CardsContactInfoDTO.java
@@ -1,5 +1,7 @@
 package com.cgnexus.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -7,5 +9,10 @@ import java.util.Map;
 
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDTO(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardsContactInfoDTO {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
