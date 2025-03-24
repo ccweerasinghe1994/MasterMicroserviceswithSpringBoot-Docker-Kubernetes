42b62c16a5cfe725750190c7b94886d6f8a1b60a
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-03-24 15:34:06 +0530

    add email and sms functions for AccountMsgDto processing with logging

--- message/src/main/java/com/cgnexus/message/functions/MessageFunctions.java
+++ message/src/main/java/com/cgnexus/message/functions/MessageFunctions.java
@@ -1,7 +1,30 @@
 package com.cgnexus.message.functions;
 
+import com.cgnexus.message.dto.AccountMsgDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
 
 @Configuration
 public class MessageFunctions {
+    private static final Logger log = LoggerFactory.getLogger(MessageFunctions.class);
+
+    @Bean
+    public Function<AccountMsgDto, AccountMsgDto> email() {
+        return accountMsgDto -> {
+            log.info("sending email with details {}", accountMsgDto);
+            return accountMsgDto;
+        };
+    }
+
+    @Bean
+    public Function<AccountMsgDto, Long> sms() {
+        return accountMsgDto -> {
+            log.info("sending sms with details {}", accountMsgDto);
+            return accountMsgDto.accountNumber();
+        };
+    }
 }
